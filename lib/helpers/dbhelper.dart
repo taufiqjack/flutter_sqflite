d288c1b4bd227981ca564d7a849baf5863286f5c
taufiqjack <taufiqdwic@gmail.com>
2021-03-22 08:09:27 +0700

    create dbHelper

---
+++ lib/helpers/dbhelper.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class DbHelper {
+  static DbHelper _dbHelper;
+  static Database _database;
+
+  DbHelper._createObject();
+
+  factory DbHelper() {
+    if (_dbHelper == null) {
+      _dbHelper = DbHelper._createObject();
+    }
+    return _dbHelper;
+  }
+  Future<Database> initDb() async {
+    //untuk menentukan nama database dan lokasi yg dibuat
+
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'catatan.db';
+
+    //create, read db
+    var todoDatabase = openDatabase(path, version: 1, onCreate: _createDb);
+
+    //mengembalikan nilai object sebagai hasil dari fungsinya
+    return todoDatabase;
+  }
+
+  //buat tabel baru dengan nama contact
+  void _createDb(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE memo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        judul TEXT,
+        detail TEXT
+      )
+    ''');
+  }
+
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDb();
+    }
+    return _database;
+  }
+
+  Future<List<Map<String, dynamic>>> select() async {
+    Database db = await this.database;
+    var mapList = await db.query('memo', orderBy: 'judul');
+    return mapList;
+  }
+}
